689d65f9cb123c0c49935a3ff8f3fc5f2e16efc9
mihaelaandr <mihaela.andreea2507@gmail.com>
2025-12-05 21:28:17 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Buton pentru meniu responsive
-document.addEventListener("DOMContentLoaded", function() {
-    const menuBtn = document.getElementById("menuToggle");
-    const nav = document.querySelector("nav");
-
-    menuBtn.addEventListener("click", () => {
-        nav.classList.toggle("active");
-    });
-});
